88edfb4ff008298ad4348388232476abb8e4f6b9
Tim Hansen <heroofgraz@gmail.com>
2019-01-13 16:18:04 -0700

    Added PBM stuff

---
+++ libctrace/main.c
@@ -0,0 +1,29 @@
+/// Just using this to test the pbm stuff for now ///
+#include "pbm.h"
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  printf("Starting\n");
+  unsigned w = 3, h = 3;
+  float r[] = { 0.0, 1.0, 1.0,
+                1.0, 0.0, 0.0,
+                1.0, 0.0, 1.0
+  };
+  float g[] = { 1.0, 1.0, 0.0,
+                0.0, 1.0, 0.0,
+                1.0, 0.0, 0.0
+  };
+  float b[] = { 1.0, 0.0, 1.0,
+                0.0, 0.0, 1.0,
+                1.0, 0.0, 0.0
+  };
+  /* Once this gets turned into an image, it should look like
+   * W B R
+   * R G B
+   * C Y M
+   */
+  ppm_t myimage = ppm_createPPM(r, g, b, w, h);
+  ppm_exportFile(myimage, "myimage.ppm");
+  printf("Ending\n");
+  return 0;
+}
