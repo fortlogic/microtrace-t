f124d2db633079caff7a52e2f2c3c632e86653c4
Matias Eyzaguirre <matias@fortlogic.net>
2019-04-03 19:12:42 -0600

    vector3 tests are finished
    Signed-off-by: Matias Eyzaguirre <matias@fortlogic.net>

--- Tests/CTraceTests/Misc.swift
+++ Tests/CTraceTests/Misc.swift
@@ -20,6 +20,19 @@ extension Float {
   }
 }
 
+extension UInt {
+  func gcd(of n: UInt) -> UInt {
+    if self == n {
+      return self
+    } else if self > n {
+      return (self - n).gcd(of: n)
+    } else if self < n {
+      return self.gcd(of:n - self)
+    }
+    preconditionFailure("Two UInts should be comparable")
+  }
+}
+
 struct PythagoreanTriple: Arbitrary {
   let a, b, c: UInt
 
@@ -27,11 +40,14 @@ struct PythagoreanTriple: Arbitrary {
     return (Float(a), Float(b), Float(c))
   }
 
-  init(_ m: UInt, _ n: UInt) {
-    assert(m > n, "m must be more than n to make a valid triple")
-    a = (m * m) - (n * n)
-    b = 2 * m * n
-    c = (m * m) + (n * n)
+  init?(_ m: UInt, _ n: UInt) {
+    if m > n {
+      a = (m * m) - (n * n)
+      b = 2 * m * n
+      c = (m * m) + (n * n)
+    } else {
+      return nil
+    }
   }
 
   static var arbitrary: Gen<PythagoreanTriple> {
@@ -42,7 +58,39 @@ struct PythagoreanTriple: Arbitrary {
       }.map {
         (pair: (UInt, UInt)) in
         let (m, n) = pair
-        return PythagoreanTriple(m, n)
+        return PythagoreanTriple(m, n)!
     }
   }
 }
+
+// Generating quads reliably is broken
+//struct PythagoreanQuadruple: Arbitrary {
+//  let a, b, c, d: UInt
+//
+//  var floats: (Float, Float, Float, Float) {
+//    return (Float(a), Float(b), Float(c), Float(d))
+//  }
+//
+//  init?(_ m: UInt, _ n: UInt, _ p: UInt, _ q: UInt) {
+//    if (m + n + p + q) % 2 == 1 {
+//      a = (m * m) + (n * n) - (p * p) - (q * q)
+//      b = 2 * ((m * q) + (n * p))
+//      c = 2 * ((n * q) - (m * p))
+//      d = (m * m) + (n * n) + (p * p) + (q * q)
+//      assert(((a * a) + (b * b) + (c * c)) == (d * d))
+//    }
+//    return nil
+//  }
+//
+//  static var arbitrary: Gen<PythagoreanQuadruple> {
+//    return Gen.zip(UInt.arbitrary, UInt.arbitrary, UInt.arbitrary, UInt.arbitrary).suchThat {
+//      (pair: (UInt, UInt, UInt, UInt)) in
+//      let (m, n, p, q) = pair
+//      return (m + n + p + q) % 2 == 1
+//      }.map {
+//        (pair: (UInt, UInt, UInt, UInt)) in
+//        let (m, n, p, q) = pair
+//        return PythagoreanQuadruple(m, n, p, q)!
+//    }
+//  }
+//}
--- Tests/CTraceTests/Vector3Tests.swift
+++ Tests/CTraceTests/Vector3Tests.swift
@@ -54,8 +54,16 @@ class Vector3Tests: XCTestCase {
     XCTAssertEqual(vec3_magnitude(vec3_unit_j), 1.0, "length of j-hat is one")
     XCTAssertEqual(vec3_magnitude(vec3_unit_k), 1.0, "length of k-hat is one")
 
-    XCTFail("length obeys the triangle property")
-    XCTFail("Pythagorean triples behave nicely")
+    property("magnitude obeys the triangle property") <- forAll {
+      (vec: vector3) in
+      return vec3_magnitude(vec) <= (vec.i.magnitude + vec.j.magnitude + vec.k.magnitude)
+    }
+
+//    property("magnitude conforms with pythagorean quadruples") <- forAll {
+//      (quad: PythagoreanQuadruple) in
+//      let (a, b, c, d) = quad.floats
+//      return vec3_magnitude(vec3_make(a, b, c)) == d
+//    }
   }
 
   func test_normalize() {
@@ -67,25 +75,42 @@ class Vector3Tests: XCTestCase {
     XCTAssertEqual(vec3_normalize(vec3_unit_j), vec3_unit_j, "i-hat normalizes to itself")
     XCTAssertEqual(vec3_normalize(vec3_unit_k), vec3_unit_k, "i-hat normalizes to itself")
 
-    XCTFail("vec and vec.normalised are in the same direction")
-    XCTFail("the result of a normalised vector has length one")
+    property("normalization preserves direction") <- forAll {
+      (vec: vector3) in
+      return (!vec.isZero) ==> {
+        let vecUnit = vec3_normalize(vec)
+        // this requires that the dot product is correct
+        return vec3_dot(vec, vecUnit).equal(to: vec3_magnitude(vec), within: 4)
+      }
+    }
+
+    property("normalisation results in magnitude one") <- forAll {
+      (vec: vector3) in
+      return (!vec.isZero) ==> {
+        return vec3_magnitude(vec3_normalize(vec)).equal(to: 1.0, within: 3)
+      }
+    }
   }
 
   func test_negate() {
     XCTAssertEqual(vec3_negate(vec3_zero), vec3_zero, "negative zero is zero")
-    XCTAssertEqual(vec3_negate(vec3_unit_i),
-                   vec3_make(-1.0, 0.0, 0.0), "negative i-hat")
-    XCTAssertEqual(vec3_negate(vec3_unit_j),
-                   vec3_make(0.0, -1.0, 0.0), "negative j-hat")
-    XCTAssertEqual(vec3_negate(vec3_unit_k),
-                   vec3_make(0.0, 0.0, -1.0), "negative k-hat")
+    XCTAssertEqual(vec3_negate(vec3_unit_i), vec3_make(-1.0, 0.0, 0.0), "negative i-hat")
+    XCTAssertEqual(vec3_negate(vec3_unit_j), vec3_make(0.0, -1.0, 0.0), "negative j-hat")
+    XCTAssertEqual(vec3_negate(vec3_unit_k), vec3_make(0.0, 0.0, -1.0), "negative k-hat")
 
     property("double negation is an identity") <- forAll {
       (vec: vector3) in
       return vec == vec3_negate(vec3_negate(vec))
     }
 
-    XCTFail("negate reverses coordinates")
+    property("negation negates coordinates") <- forAll {
+      (vec: vector3) in
+      let neg = vec3_negate(vec)
+
+      return ((-vec.i == neg.i) <?> "i")
+        ^&&^ ((-vec.j == neg.j) <?> "j")
+        ^&&^ ((-vec.k == neg.k) <?> "k")
+    }
   }
 
   func test_scale() {
@@ -99,50 +124,151 @@ class Vector3Tests: XCTestCase {
       return vec3_scale(0.0, vec) == vec3_zero
     }
 
-    XCTFail("scaling basis i")
-    XCTFail("scaling basis j")
-    XCTFail("scaling basis k")
-    XCTFail("scaling by one is identity")
-    XCTFail("scaling by -1 is negation")
-    XCTFail("scaling scales")
+    property("scaling by 1 is the identity") <- forAll {
+      (v: vector3) in
+      return vec3_scale(1.0, v) == v
+    }
+
+    property("scaling by -1 negates") <- forAll {
+      (vec: vector3) in
+      // this requires that negation is correct
+      return vec3_scale(-1.0, vec) == vec3_negate(vec)
+    }
+
+    property("scaling scales coordinates") <- forAll {
+      (s: Float, vec: vector3) in
+      let scaled = vec3_scale(s, vec)
+
+      return ((s * vec.i == scaled.i) <?> "i")
+        ^&&^ ((s * vec.j == scaled.j) <?> "j")
+        ^&&^ ((s * vec.k == scaled.k) <?> "k")
+    }
   }
 
   func test_dot() {
-    XCTFail("dot zero is zero")
-    XCTFail("basis vectors ij are orthogonal")
-    XCTFail("basis vectors jk are orthogonal")
-    XCTFail("basis vectors ki are orthogonal")
-    XCTFail("dotting with basis vector i extracts coordinates")
-    XCTFail("dotting with basis vector j extracts coordinates")
-    XCTFail("dotting with basis vector k extracts coordinates")
-    XCTFail("dot commutes")
-    XCTFail("scaling one of the vectors scales the dot product")
-    XCTFail("mag a * mag b * cos theta")
+    XCTAssertEqual(vec3_dot(vec3_zero, vec3_zero), 0.0, "zero dot zero is zero")
+    XCTAssertEqual(vec3_dot(vec3_unit_i, vec3_unit_j), 0.0, "i-hat should be orthogonal to j-hat")
+    XCTAssertEqual(vec3_dot(vec3_unit_j, vec3_unit_k), 0.0, "j-hat should be orthogonal to k-hat")
+    XCTAssertEqual(vec3_dot(vec3_unit_k, vec3_unit_i), 0.0, "k-hat should be orthogonal to i-hat")
+
+    property("dotting with i-hat extracts the i component") <- forAll {
+      (vec: vector3) in
+      return vec3_dot(vec, vec3_unit_i) == vec.i
+    }
+
+    property("dotting with j-hat extracts the j component") <- forAll {
+      (vec: vector3) in
+      return vec3_dot(vec, vec3_unit_j) == vec.j
+    }
+
+    property("dotting with k-hat extracts the k component") <- forAll {
+      (vec: vector3) in
+      return vec3_dot(vec, vec3_unit_k) == vec.k
+    }
+
+    property("the dot product is commutative") <- forAll {
+      (vec1: vector3, vec2: vector3) in
+      return vec3_dot(vec1, vec2) == vec3_dot(vec2, vec1)
+    }
+
+    // The following are copied from the vector2 tests. Those tests are unreliable, implementing them
+    // for vector3 would be premature until the vector2 versions are reliable.
+
+    //    property("the dot product scales") <- forAll {
+    //      (s1: Float, s2: Float, vec1: vector2, vec2: vector2) in
+    //      let dotScale = vec2_dot(vec2_scale(s1, vec1), vec2_scale(s2, vec2))
+    //      let scaleDot = s1 * s2 * vec2_dot(vec1, vec2)
+    //
+    //      return (!dotScale.isZero && !scaleDot.isZero) ==> {
+    //        return dotScale.equal(to: scaleDot, within: 64)
+    //      }
+    //    }
+
+    //    property("dot product agrees with geometric interpretation") <- forAll {
+    //      (vec1: vector2, vec2: vector2) in
+    //      return (!vec1.isZero && !vec2.isZero) ==> {
+    //        let geoDot = vec2_magnitude(vec1) * vec2_magnitude(vec2) * cos((vec1.angle - vec2.angle).magnitude)
+    //        return vec2_dot(vec1, vec2).equal(to: geoDot, within: 256)
+    //      }
+    //    }
   }
 
   func test_distance() {
-    XCTFail("distance from zero is magnitude")
-    XCTFail("triangle property")
-    XCTFail("offset pythagorean triples")
-    XCTFail("distance commutes")
+    property("distance from 0 is magnitude") <- forAll {
+      (v: vector3) in
+      return vec3_distance(v, vec3_zero) == vec3_magnitude(v)
+    }
+
+    property("triangle property holds") <- forAll {
+      (a: vector3, b: vector3, c: vector3) in
+      return vec3_distance(a, c) <= (vec3_distance(a, b) + vec3_distance(b, c))
+    }
+    
+    property("distance is commutative") <- forAll {
+      (vec1: vector3, vec2: vector3) in
+      return vec3_distance(vec1, vec2) == vec3_distance(vec2, vec1)
+    }
+
+//    property("distance agrees with pythagorean quadruples") <- forAll {
+//      (i: UInt, j: UInt, k: UInt, quad: PythagoreanQuadruple) in
+//      // this requires vec2_add to be correct
+//      let (a, b, c, d) = quad.floats
+//      let v1 = vec3_make(Float(i), Float(j), Float(k))
+//      let v2 = vec3_add(v1, vec3_make(a, b, c))
+//      return vec3_distance(v1, v2) == d
+//    }
   }
 
   func test_add() {
-    XCTFail("zero is additive identity")
+    property("zero is the additive identity") <- forAll {
+      (v: vector3) in
+      return vec3_add(v, vec3_zero) == v
+    }
+
+    property("a+(-a)=0") <- forAll {
+      (v: vector3) in
+      return vec3_add(v, vec3_negate(v)) == vec3_zero
+    }
 
     property("addition commutes") <- forAll {
       (vec1: vector3, vec2: vector3) in
       return vec3_add(vec1, vec2) == vec3_add(vec2, vec1)
     }
 
-    XCTFail("coordinates get added")
+    property("addition adds coordinates") <- forAll {
+      (vec1: vector3, vec2: vector3) in
+      let sum = vec3_add(vec1, vec2)
+      return ((sum.i == (vec1.i + vec2.i)) <?> "i")
+        ^&&^ ((sum.j == (vec1.j + vec2.j)) <?> "j")
+        ^&&^ ((sum.k == (vec1.k + vec2.k)) <?> "k")
+    }
   }
 
   func test_subtract() {
-    XCTFail("zero is identity")
-    XCTFail("negationg cancels out")
-    XCTFail("|a-b| = |b-a|")
-    XCTFail("coordinates subtract")
+    property("zero is the subtractive identity") <- forAll {
+      (v: vector3) in
+      return vec3_subtract(v, vec3_zero) == v
+    }
+    
+    property("a - a = 0") <- forAll {
+      (vec: vector3) in
+      return vec3_subtract(vec, vec) == vec3_zero
+    }
+
+    property("|a-b|=|b-a|") <- forAll {
+      (v1: vector3, v2: vector3) in
+      let diff1 = vec3_magnitude(vec3_subtract(v1, v2))
+      let diff2 = vec3_magnitude(vec3_subtract(v2, v1))
+      return diff1 == diff2
+    }
+
+    property("subtraction subtracts coordinates") <- forAll {
+      (vec1: vector3, vec2: vector3) in
+      let diff = vec3_subtract(vec1, vec2)
+      return ((diff.i == (vec1.i - vec2.i)) <?> "i")
+        ^&&^ ((diff.j == (vec1.j - vec2.j)) <?> "j")
+        ^&&^ ((diff.k == (vec1.k - vec2.k)) <?> "k")
+    }
   }
 
 //  func test_addn() {
@@ -158,7 +284,9 @@ class Vector3Tests: XCTestCase {
 ///
 
 extension vector3 {
-  
+  var isZero: Bool {
+    return self == vec3_zero
+  }
 }
 
 extension vector3: Arbitrary, CustomStringConvertible, Equatable {
