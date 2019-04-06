18bf1a3c3aeb10f5dbaecb7613556da7d5ef4244
Matias Eyzaguirre <matias@fortlogic.net>
2019-04-05 19:37:21 -0600

    Some modifications for linux people
    Signed-off-by: Matias Eyzaguirre <matias@fortlogic.net>

--- Package.swift
+++ Package.swift
@@ -7,9 +7,12 @@ let package = Package(
   name: "µtraceT",
   products: [
     // Products define the executables and libraries produced by a package, and make them visible to other packages.
+//    .library(
+//      name: "µtraceT",
+//      targets: ["µtraceT"]),
     .library(
-      name: "µtraceT",
-      targets: ["µtraceT"]),
+      name: "CTrace",
+      targets: ["CTrace"])
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
@@ -19,13 +22,13 @@ let package = Package(
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-    .target(
-      name: "µtraceT",
-      dependencies: []),
-    .testTarget(
-      name: "µtraceTTests",
-      dependencies: ["µtraceT"]
-    ),
+//    .target(
+//      name: "µtraceT",
+//      dependencies: []),
+//    .testTarget(
+//      name: "µtraceTTests",
+//      dependencies: ["µtraceT"]
+//    ),
     .target(
       name: "CTrace"
     ),
